daee6e565f50e7096b03413c01dda9e71f7b67a4
michaeljumah <michaeljumah8@gmail.com>
2024-07-03 19:27:57 +0300

    systematic strategy to apply for solving problems

--- LinearBinarySearch/quiz1.py
+++ LinearBinarySearch/quiz1.py
@@ -1,3 +1,15 @@
 '''
 QUESTION 1: Alice has some cards with numbers written on them. she arranges the cards in decreasing order, and lays them out face down in a sequence on a table. she challanges Bob to pick out the card containing a given number by turning over some few cards as possible. write a function to help Bob locate the card
 '''
+
+"""
+systematic strategy to apply for solving problems:
+    1. State the problem clearly. identify the inputs and outputs formats
+    2. come up with some example inputs and outputs. try tocover all possible edge cases.
+    3. come up with the correct solution for the problem. state it in plain english.
+    4. impliment the solutions and test it using example inputs. fix bugs, if any. 
+    5. analyse the algorithms complexity and identify the ineffiencies, if any.
+    6. apply the right technique to overcome the inefficieny. repeart step 3 to 6
+    
+"""
+
