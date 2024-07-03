41aeef9bd1494de5b8426c582cd9cace1143d7d5
michaeljumah <michaeljumah8@gmail.com>
2024-07-04 00:25:47 +0300

    different test cases in a dictonary

--- LinearBinarySearch/quiz1.py
+++ LinearBinarySearch/quiz1.py
@@ -31,13 +31,77 @@ def LocateCard(cards, query):
     """
         test cases will be represented as DICTIONARIES countaing two keys, input and outputs 
     """
-test = {
+tests = []
+
+#tests.append(tests)
+
+
+tests.append = {
     'input': {
         'cards': [13,11,10,7,4,3,1,0],
         'query': 7
         },
     'output': 3
 }
+
+tests.append = {
+    'input': {
+        'cards': [13,11,10,7,4,3,1,0],
+        'query': 1
+        },
+    'output': 6
+}
+
+tests.append = {
+    'input': {
+        'cards': [4,3,1,-1],
+        'query': 4
+        },
+    'output': 0
+}
+
+tests.append = {
+    'input': {
+        'cards': [3,-1,-9,-127],
+        'query': -127
+        },
+    'output': 3
+}
+
+tests.append = {
+    'input': {
+        'cards': [13,11,10,7,4,3,1,0],
+        'query': 7
+        },
+    'output': 3
+}
+
+tests.append = {
+    'input': {
+        'cards': [6],
+        'query': 6
+        },
+    'output': 0
+}
+
+tests.append = {
+    'input': {
+        'cards': [8,8,6,6,6,6,6,3,4,4,2,2,2,0,0,0,0],
+        'query': 3
+        },
+    'output': -7
+}
+
+tests.append = {
+    'input': {
+        'cards': [],
+        'query': 7
+        },
+    'output': -1
+}
+
+
+ptint(tests)
 
 LocateCard(test['input']['cards'], test['input']['query']) == test['output']
 
