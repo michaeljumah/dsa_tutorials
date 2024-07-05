1812ddb5f1b5e6e7bc0a21ffa57fdb471ed3bea6
michaeljumah <michaeljumah8@gmail.com>
2024-07-05 22:19:43 +0300

    to be continued tomorrow

--- PascalsTriangle/PascalsTriangle.py
+++ PascalsTriangle/PascalsTriangle.py
@@ -5,6 +5,10 @@
 
 class Solution:
     def pascal_triangle(self,numRows: int) -> List[list[int]]:
-        res = [[1]]
+        res = [[1]]   #the first row
         
-        
+        #building the rest of the rows
+        for i in range(numRows - 1):
+            temp = [0] + res[-1] + [0]
+            for j in range(len(res[-1]) + 1):
+                
