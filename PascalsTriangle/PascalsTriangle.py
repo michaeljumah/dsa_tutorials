17e0bd97a62d4cbc337e4ecf22fd1beb0ae66b6a
michaeljumah <michaeljumah8@gmail.com>
2024-07-05 22:15:36 +0300

    init

--- PascalsTriangle/PascalsTriangle.py
+++ PascalsTriangle/PascalsTriangle.py
@@ -2,3 +2,9 @@
     given an integer numRows return the first numRows of Pascal's Triangle
     in pascal's triangle each number is the sum of the two numbers directly above it as shown
 """
+
+class Solution:
+    def pascal_triangle(self,numRows: int) -> List[list[int]]:
+        res = [[1]]
+        
+        
