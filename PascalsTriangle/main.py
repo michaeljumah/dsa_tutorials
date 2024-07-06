b2d9e1f3fe7f889d2a872d2122e625c95b1eef25
michaeljumah <michaeljumah8@gmail.com>
2024-07-07 00:34:17 +0300

    no message

--- PascalsTriangle/main.py
+++ PascalsTriangle/main.py
@@ -7,14 +7,9 @@ main file
 #PascalTriangle = __import__(PascalTriangle').PascalTriangle
 
 def print_triangle(triangle):
-"""
-    print the triangle
-    Args:
-    triangle (_type_): _description
-"""
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
         
-if __name__ == "__main__":
-    print_triangle(PascalTriangle(5))
+        if __name__ == "__main__":
+            print_triangle(PascalTriangle(5))
             
