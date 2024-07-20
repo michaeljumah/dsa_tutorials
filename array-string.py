ee91bae67dc342f130aef878359d40702865da65
michaeljumah <michaeljumah8@gmail.com>
2024-07-21 02:40:30 +0300

    worse scenario

--- array-string.py
+++ array-string.py
@@ -2,3 +2,18 @@
 nums = [1,2,3,4,5]
 print (nums[0])
 print (nums[-5])
+print (nums[len(nums) - 1])
+print(nums[::-1])
+
+print ("------------------------------------")
+def addtwo(arr, tar):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if (arr[i]+arr[j]) == tar:
+                return [arr[i], arr[j]]
+    
+
+
+arr = [1,2,3,4,5,6,7]
+tar = 5
+print(addtwo(arr, tar))
