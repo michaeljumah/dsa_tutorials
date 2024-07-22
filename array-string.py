f641094b15c57ab3cfb3d58db199e55bee805ab9
michaeljumah <michaeljumah8@gmail.com>
2024-07-22 23:25:18 +0300

    range(len(arr))

--- array-string.py
+++ array-string.py
@@ -7,13 +7,15 @@ print(nums[::-1])
 
 print ("----------------------------------------------")
 def addtwo(arr, tar):
-    for i in range(len(arr)):
+    """for i in range(len(arr)):
         for j in range(i+1, len(arr)):
             if (arr[i]+arr[j]) == tar:
-                return [arr[i], arr[j]]
+                return [arr[i], arr[j]]"""
+    
     
 
 
-arr = [1,2,3,4,5,6,7]
+arr = [2,3,4,5,6,7]
+print('range of length of arr:', range(len(arr)))
 tar = 7
-print(addtwo(arr, tar))
+#print(addtwo(arr, tar))
